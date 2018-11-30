f3f74dd7a7153ad50c696291e0e5258c84a4e7f2
Lam Haoyin <42838307+lamhaoyin@users.noreply.github.com>
2018-11-30 22:54:05 +0800

    Update lxde.sh

--- lxde.sh
+++ lxde.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-echo "---------------------------------------------------------------------------------------------------------------------" 
-echo "----LXDE+VNC+Firefox  V1.0 Powered by Tomy----"
-echo "    URL:http://blog.ievo.top"
-echo "    Telegram:@bestomy"
-echo "---------------------------------------------------------------------------------------------------------------------" 
-echo Press any key to continue! Exit with 'Ctrl'+'C' !
 cd /root
-apt-get update -y
-apt-get install xorg -y
-apt-get install lxde-core -y
-apt-get install tightvncserver -y
+apt update -y
+apt install xorg -y
+apt install lxde-core -y
+apt install tightvncserver -y
 
 tightvncserver :1
 tightvncserver -kill :1
@@ -21,15 +15,3 @@ export XKL_XMODMAP_DISABLE=1
 /etc/X11/Xsession
 lxterminal &
 /usr/bin/lxsession -s LXDE & " > ~/.vnc/xstartup
-
-tightvncserver :1
-
-echo "Install firefox and Chinese support"
-apt-get install iceweasel -y
-apt-get install lxde -y
-apt-get install ttf-arphic-uming -y
-apt-get install xfonts-intl-chinese -y
-apt-get install xfonts-wqy -y
-apt-get install ttf-arphic-ukai ttf-arphic-uming ttf-arphic-gbsn00lp ttf-arphic-bkai00mp ttf-arphic-bsmi00lp -y
-echo "------------Finished!--------------"
-echo "-------安装完成 祝您搞机愉快-------"
